da961ba005790765bd81ee6002ff52ae315eadfb
Ricky Kumar <anitasah926@gmail.com>
2024-07-07 00:33:30 +0530

    iiii

--- FE/vite-project/src/App.jsx
+++ FE/vite-project/src/App.jsx
@@ -3,8 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Register from "./Register";
 import Login from "./Login";
 import { Link } from "react-router-dom";
-import Create from "./Create";
-import Opps from './Opps';
+
 
 
 function App() {
@@ -13,14 +12,11 @@ function App() {
     <nav>
       <Link to="/register">register</Link>
       <Link to="/login">Log-in</Link>
-      <Link to="/create">TODO</Link>
-      <Link to="/opps">opps</Link>
+
     </nav>
       <Routes>
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login/>} />
-        <Route path="/create" element={<Create/>} />
-        <Route path="/opps" element={<Opps/>} />
       </Routes>
     </>
   );
